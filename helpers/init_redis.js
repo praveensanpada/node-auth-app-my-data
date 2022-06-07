a752a05fc1ea85f6c6920f7fd8bebe89bb84bfad
praveen_sanpada <praveen.sanpada@aurigait.com>
2022-06-07 14:21:30 +0530

    my app

---
+++ helpers/init_redis.js
@@ -0,0 +1,29 @@
+const redis = require('redis')
+
+const client = redis.createClient({
+  port: 6379,
+  host: 'nodedb-001.hhtihc.0001.usw2.cache.amazonaws.com',
+  // password: "password"
+})
+
+client.on('connect', () => {
+  console.log('Client connected to redis...')
+})
+
+client.on('ready', () => {
+  console.log('Client connected to redis and ready to use...')
+})
+
+client.on('error', (err) => {
+  console.log(err.message)
+})
+
+client.on('end', () => {
+  console.log('Client disconnected from redis')
+})
+
+// process.on('SIGINT', () => {
+//   client.quit()
+// })
+
+module.exports = client
